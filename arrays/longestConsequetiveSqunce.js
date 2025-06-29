de16b4db10ba9f1622b95910dbb8a5e9a2a01a7e
bhushandeore1 <pbhushandeore@gmail.com>
2025-06-29 16:17:11 +0530

    bruteforce approach for longest consequestive sequence

---
+++ arrays/longestConsequetiveSqunce.js
@@ -0,0 +1,24 @@
+// Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+// You must write an algorithm that runs in O(n) time.
+
+// Input: nums = [100,4,200,1,3,2]
+// Output: 4
+// Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+
+
+//approach 1 
+//this is bruteforce approach as it is taking time complexity as 0(n2) and also it is exceeding time limit
+function longestConsecutive(nums) {
+    let maxLength=0;
+    for(let i=0;i<nums.length;i++){
+        let currentNum=nums[i];
+        let currentStreak=1;
+        while(nums.includes(currentNum+1)){
+            currentNum+=1;
+            currentStreak+=1;
+        }
+        maxLength=Math.max(maxLength,currentStreak);
+    }
+    return maxLength;
+}
