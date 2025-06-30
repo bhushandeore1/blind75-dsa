2eeedf9c6164d61057de2919b6557d04323290a9
punambhamare11 <punambhamare11@gmail.com>
2025-06-30 22:43:53 +0530

    added logic

--- arrays/duplicatesInArray.js
+++ arrays/duplicatesInArray.js
@@ -1,5 +1,19 @@
 //find duplicates in array or length of the duplicates 
 
 function findduplicates(nums){
-
+     const numSet=new Set();
+     const duplicates=new Set();
+     for(let num of nums){
+          if(numSet.has(num)){
+               duplicates.add(num);
+          }else{
+               numSet.add(num)
+          }
+     }
+     return [...duplicates];
 }
+
+findduplicates([1,1,3,4,4,3]);
+
+
+
