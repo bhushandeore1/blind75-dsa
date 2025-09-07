bd29847f251de6dd3161e2cd10a3013ceee86d1e
bhushandeore1 <pbhushandeore@gmail.com>
2025-09-07 17:43:09 +0530

    added polyfill implementation for Array.prototype.myFlat to flatten nested arrays

--- arrays/flattenedNestedArray.js
+++ arrays/flattenedNestedArray.js
@@ -13,3 +13,29 @@ function flattenedNestedArr(arr,depth){
 
 
 const nestedarr=[[1,3],[2,7,13,9],[12,10,[90,[2,9,4],10]],152]; ///[1,3,2,7,13,9,90,2,9,4,10,152];
+
+flattenedNestedArr(nestedarr,3)
+
+
+//implement arr.flat.polyfill
+
+if(!Array.prototype.myFlat){
+    Array.prototype.myFlat=function(depth=1){
+
+        const result=[];
+
+        function flattenArr(arr,depth){
+            arr.forEach(num=>{
+                if(Array.isArray(arr) && depth>1){
+                    flattenArr(num,depth-1);
+                }else{
+                    result.push(num);
+                }
+            })
+        }
+        flattenArr(this,depth);
+        return result;
+    }
+}
+
+const res=nestedarr.myFlat();
