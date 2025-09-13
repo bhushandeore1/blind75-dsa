dcd0161a95e4be670e3c87de89486ab546b58a01
bhushandeore1 <pbhushandeore@gmail.com>
2025-09-13 16:46:28 +0530

    added implementation of Fisher-Yates shuffle algorithm to randomly shuffle an array

---
+++ arrays/shuffleAnArrayRandomly.js
@@ -0,0 +1,12 @@
+//this is code for shuffle the array randomly
+//this is also called fisher-yates shuffle
+
+function shuffle(arr){
+    for(let i=arr.length-1; i>0; i--){
+        const j=Math.floor(Math.random() * (i+1));
+        [arr[i],arr[j]]=[arr[j],arr[i]];
+    }
+    return arr;
+}
+
+shuffle([1,2,3,4,5,6]);
